eaca57d71c2a52b87d18690c41f8530d608768ba
Nesrine <nesrinakon@gmail.com>
2017-11-02 22:44:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,6 +13,7 @@ function addElementToEndOfArray(array, element){
 }
 function destructivelyAddElementToEndOfArray(array, element){
   array = [...array, element]
+  return array
 }
 function accessElementInArray(array, index){
   return array[index]
