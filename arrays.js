4ed0a10497415d00e2ce45f61ece7dadf442fee3
Nesrine <nesrinakon@gmail.com>
2017-11-02 22:41:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
  function destructivelyAddElementToBeginningOfArray(array, element){
-   array.unshift(element)
+   array = [element, ...array]
  }
 function addElementToEndOfArray(array, element){
   return [...array, element]
